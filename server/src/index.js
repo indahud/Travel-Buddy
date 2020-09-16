f740d0a500fb036161828f317ba932037ef3c716
indahud <indahud@protonmail.com>
2020-09-16 15:04:47 +0530

    bodyparser, mongoose and endpoint added

--- server/src/index.js
+++ server/src/index.js
@@ -2,21 +2,36 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const logs = require('./api/logs');
+
+require('dotenv').config();
 
 const middlewares = require('./middlewares');
 
 const app = express();
+
+// eslint-disable-next-line quotes
+mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/travel-log`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 app.use(morgan('common'));
 app.use(helmet());
 app.use(cors({
-  origin: 'http://localhost:1337',
+  origin: process.env.CORS_ORIGIN,
 }));
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json({
     message: 'Hello World',
   });
 });
+
+app.use('/api/logs', logs);
 
 // Error Handling
 app.use(middlewares.notFound);
@@ -25,5 +40,6 @@ app.use(middlewares.errorHandler);
 const port = process.env.PORT || 1337;
 
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`Running on http://localhost:${port}`);
 });
