f94bbc07088eb6366f2a4fe3ee0f5b85eb0530ec
indahud <indahud@protonmail.com>
2020-09-16 15:05:18 +0530

    added routes

---
+++ server/src/api/logs.js
@@ -0,0 +1,28 @@
+const { Router } = require('express');
+const LogEntry = require('../models/LogEntry');
+
+const router = Router();
+
+router.get('/', async (req, res, next) => {
+  try {
+    const entries = await LogEntry.find();
+    res.json(entries);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    const logEntry = new LogEntry(req.body);
+    const createdAt = await logEntry.save();
+    res.json(createdAt);
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+      res.status(422);
+    }
+    next(error);
+  }
+});
+
+module.exports = router;
